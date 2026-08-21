7ac462ce462bc4d01ed302cbc4e736c9fed0f111
lemming104 <lemming104@gmail.com>
2026-08-21 10:11:49 -0700

    Manually set .dylib search path on MacOS

--- ZMusicDemo/Program.cs
+++ ZMusicDemo/Program.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.IO;
+    using OpenTK.Audio.OpenAL;
 
     public class Program
     {
@@ -15,6 +16,27 @@
                 return;
             }
 
+            if (OperatingSystem.IsMacOS())
+            {
+                // There are two possible search paths depending on whether we are in a non runtime-specific build or not.
+                string noRuntimePath = Path.Combine(AppContext.BaseDirectory, "libopenal.dylib");
+                string runtimePath = Path.Combine(AppContext.BaseDirectory, "runtimes/osx-arm64/native/libopenal.dylib");
+
+                if (Path.Exists(runtimePath))
+                {
+                    OpenALLibraryNameContainer.OverridePath = runtimePath;
+                }
+                else if (Path.Exists(noRuntimePath))
+                {
+                    OpenALLibraryNameContainer.OverridePath = noRuntimePath;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Cannot find OpenAL-Soft");
+                }
+            }
+
+            Console.WriteLine($"Asking OpenTK to use OpenAL library: {OpenALLibraryNameContainer.OverridePath}");
             SimplePlayer.Play(args);
         }
     }
